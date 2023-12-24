1868e56c6c7cbb0aec5697bb7aab816da4ab1d79
EladNLG <44613424+EladNLG@users.noreply.github.com>
2023-12-24 21:14:52 +0200

    1.1
    - add configuration support through pluginconfigurator
    - remove unnecessary null checks, clean up code
    - fix lotsa bugs

--- Healthbar.cs
+++ Healthbar.cs
@@ -8,8 +8,15 @@ using System.Linq;
 using Healthbars;
 using UnityEngine.ProBuilder.MeshOperations;
 
+public enum DisplayMode
+{
+    Normalized,
+    Accurate
+}
+
 public class Healthbar : MonoBehaviour
 {
+    protected static Dictionary<EnemyIdentifier, Healthbar> activeHealthbars;
     public TextMeshProUGUI label;
     public Image bar;
     public Image barBG;
@@ -20,8 +27,6 @@ public class Healthbar : MonoBehaviour
     public EnemyIdentifier enemy;
     public Transform head;
 
-    public Color bossColor = new(1f, 0.28f, 0.28f);
-    public Color normalColor = new(1, 0.82f, 0.28f);
 
     float timeSinceDamaged = 99.9f;
     float lastHealth = 0f;
@@ -46,22 +51,23 @@ public class Healthbar : MonoBehaviour
         head = enemy.GetComponentInChildren<SeasonalHats>(includeInactive: true)?.transform;
         if (head == null)
             head = enemy.transform;
-
-        if (enemy.isBoss)
-            bar.color = bossColor;
-        else
-            bar.color = normalColor;
     }
 
     void Update()
     {
+        timeSinceDamaged += Time.deltaTime;
         canvasGroup.alpha = Mathf.InverseLerp(4f, 3.5f, timeSinceDamaged);
         if (enemy == null || (enemy.health <= 0f && canvasGroup.alpha <= 0f))
         {
             Destroy(gameObject);
             return;
         }
-        Vector3 point = MonoSingleton<CameraController>.Instance.cam.WorldToScreenPoint(head.position + head.localScale.y * head.up * 0.5f);
+        // heads can get destroyed?
+        if (head == null)
+            head = enemy.transform;
+
+        // calc screen position
+        Vector3 point = MonoSingleton<CameraController>.Instance.cam.WorldToScreenPoint(head.position + head.localScale.y * Vector3.up * 0.5f);
         if (point.z < 0f)
         {
             canvasGroup.alpha = 0f;
@@ -69,36 +75,90 @@ public class Healthbar : MonoBehaviour
         rectTransform.position = point + new Vector3(0, 12, 0);
         
         // update timeSinceDamaged
-        if (lastHealth != enemy.health && enemy.health > 0f)
+        if (lastHealth != enemy.health && (lastHealth > 0f || enemy.health > 0f))
         {
+            float actualHealth = Mathf.Max(lastHealth, 0f);
             if (timeSinceDamaged <= 4f)
-                damage += Mathf.Min(lastHealth - enemy.health, enemy.health);
+                damage += Mathf.Min(lastHealth - enemy.health, actualHealth);
             else
-                damage = Mathf.Min(lastHealth - enemy.health, enemy.health);
+                damage = Mathf.Min(lastHealth - enemy.health, actualHealth);
             lastHealth = enemy.health;
             timeSinceDamaged = 0f;
         }
 
-        switch (Mathf.Sign(damage))
-        {
-            case 1:
-                damageLabel.text = $"-{Mathf.FloorToInt(damage * 100f)}";
-                break;
-            case 0:
-                damageLabel.text = $"-0";
-                break;
-            case -1:
-                damageLabel.text = $"<color=#0F0>+{Mathf.FloorToInt(damage * -100f)}"; // -100 to convert it to positive.
-                break;
-        }
+        // update damage label
 
         // update UI
         bar.fillAmount = enemy.health / maxHealth;
+        float barBGHealth = barBG.fillAmount * maxHealth;
         barBG.fillAmount = Mathf.Max(bar.fillAmount, 
-            Mathf.MoveTowards(barBG.fillAmount, bar.fillAmount, Time.deltaTime * Mathf.Lerp(0.1f, 0.3f, barBG.fillAmount - bar.fillAmount))
+            Mathf.MoveTowards(barBG.fillAmount, bar.fillAmount, Time.deltaTime * Mathf.Lerp(1f, 5f, (barBGHealth - enemy.health)) / maxHealth)
         );
-        label.text = $"{Mathf.CeilToInt(Mathf.Max(enemy.health * 100f, 0f))}<space=0.25em><size=75%><color=#FFFA>{Mathf.CeilToInt(maxHealth * 100f)}";
-        timeSinceDamaged += Time.deltaTime;
+        switch (Plugin.displayMode.value)
+        {
+            case DisplayMode.Normalized:
+                label.text = $"{Mathf.CeilToInt(Mathf.Max(enemy.health * 100f, 0f))}<space=0.25em><size=75%><color=#FFFA>{Mathf.CeilToInt(maxHealth * 100f)}";
+                break;
+            case DisplayMode.Accurate:
+                string maxHealthStr = (Mathf.CeilToInt(maxHealth * 100f) / 100f).ToString();
+                string healthStr = (Mathf.CeilToInt(Mathf.Max(enemy.health * 100f, 0f)) / 100f).ToString("0.00");
+                label.text = $"{healthStr}<space=0.25em><size=75%><color=#FFFA>{maxHealthStr}";
+                break;
+        }
+
+        UpdateDamageLabel();
+
+        switch (true)
+        {
+            case true when enemy.blessed:
+                bar.color = Plugin.blessedColor.value;
+                break;
+            case true when enemy.isBoss:
+                bar.color = Plugin.bossColor.value;
+                break;
+            default:
+                bar.color = Plugin.normalColor.value;
+                break;
+        }
+    }
+
+    void UpdateDamageLabel()
+    {
+        switch (Plugin.displayMode.value)
+        {
+            case DisplayMode.Normalized:
+                switch (Mathf.Sign(damage))
+                {
+                    case 1:
+                        damageLabel.text = $"-{Mathf.FloorToInt(damage * 100f)}";
+                        break;
+                    case 0:
+                        damageLabel.text = $"-0";
+                        break;
+                    case -1:
+                        damageLabel.text = $"<color=#0F0>+{Mathf.FloorToInt(damage * -100f)}"; // -100 to convert it to positive.
+                        break;
+                }
+                break;
+            case DisplayMode.Accurate:
+                switch (Mathf.Sign(damage))
+                {
+                    case 1:
+                        damageLabel.text = $"-{Mathf.FloorToInt(damage * 100f) / 100f}";
+                        break;
+                    case 0:
+                        damageLabel.text = $"-0";
+                        break;
+                    case -1:
+                        damageLabel.text = $"<color=#0F0>+{Mathf.FloorToInt(damage * -100f) / 100f}"; // -100 to convert it to positive.
+                        break;
+                }
+                break;
+        }
+    }
+
+    void OnDestroy()
+    {
 
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -3,6 +3,9 @@ using HarmonyLib;
 using Jotunn.Utils;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using PluginConfig.API;
+using PluginConfig.API.Fields;
+using JetBrains.Annotations;
 
 namespace Healthbars
 {
@@ -12,23 +15,41 @@ namespace Healthbars
         public static Plugin instance;
         public static GameObject healthbar;
         public static Canvas canvas;
+        
+        // configuration stuff
+        internal static PluginConfigurator config;
+        internal static EnumField<DisplayMode> displayMode;
+
+        public static ColorField bossColor;
+        public static ColorField normalColor;
+        public static ColorField blessedColor;
+
         private void Awake()
         {
             instance = this;
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            // load included bundle
             AssetBundle bundle = AssetUtils.LoadAssetBundleFromResources("healthbars", System.Reflection.Assembly.GetExecutingAssembly());
-            if (bundle == null)
-                throw new System.Exception("tHE FUCK 3");
+
+            // healthbar prefab
             healthbar = bundle.LoadAsset<GameObject>("Healthbar");
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loade2d!");
-            if (healthbar == null)
-                throw new System.Exception("tHE FUCK");
+
+            // canvas prefab
             canvas = Instantiate(bundle.LoadAsset<GameObject>("HealthbarCanvas")).GetComponent<Canvas>();
+            // cba to reinstantiate every scene change tbh
             DontDestroyOnLoad(canvas);
-            if (canvas == null)
-                throw new System.Exception("tHE FUCK 2");
-            Logger.LogInfo($"Canvas Ready: {canvas != null}");
+
+            // PluginConfigurator
+            config = PluginConfigurator.Create("Healthbars", "me.eladnlg.healthbars");
+            config.SetIconWithURL("https://raw.githubusercontent.com/EladNLG/UltrakillHealthbars/main/assets/icon.png");
+            displayMode = new EnumField<DisplayMode>(config.rootPanel, "Display Mode", "displayMode", DisplayMode.Normalized);
+            bossColor = new ColorField(config.rootPanel, "Boss Healthbar Color", "bossColor", new Color(1f, 0.28f, 0.28f));
+            normalColor = new ColorField(config.rootPanel, "Regular Healthbar Color", "normalColor", new Color(1f, 0.82f, 0.28f));
+            blessedColor = new ColorField(config.rootPanel, "Blessed Healthbar Color", "blessedColor", new Color(0.28f, 0.82f, 1f));
+            
             Harmony patcher = new Harmony("me.eladnlg.healthbars");
             patcher.PatchAll();
 
